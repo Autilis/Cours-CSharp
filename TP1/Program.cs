d0ff09b10a5a9cd8c33eb0249a2658aca7f662ad
Matthieu DE LAJARTE <matthieu.delajarte2020@campus-eni.fr>
2020-09-15 11:31:19 +0200

    test

--- TP1/Program.cs
+++ TP1/Program.cs
@@ -11,11 +11,13 @@ namespace TP1
     {
         static void Main(string[] args)
         {
-            var formes = new List<Forme>();
+            var formes = new List<Forme>(); 
             formes.Add(new Cercle { Rayon = 3 });
             formes.Add(new Rectangle { Largeur = 3, Longueur = 4 });
             formes.Add(new Carre { Longueur = 3 });
             formes.Add(new Triangle { A = 4, B = 5, C = 6 });
+            // test
+            // grger
 
             foreach (var forme in formes)
             {
